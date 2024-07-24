cb9b056d174b405bac7bdf810034ac67d764e184
Ahmed <ahmed15ayman7ahmed2002@gmail.com>
2024-07-24 08:10:54 +0300

    update

--- app/(root)/services/Service.tsx
+++ app/(root)/services/Service.tsx
@@ -1,25 +1,31 @@
-"use client"
-import LoanApplication from '@/components/shared/LoanApplication'
-import Services from '@/components/shared/Services'
-import ServicesSection from '@/components/shared/ServicesSection'
-import { servicesArray } from '@/constant/services'
-import { useSearchParams } from 'next/navigation'
-import React from 'react'
+"use client";
+import LoanApplication from "@/components/shared/LoanApplication";
+import Services from "@/components/shared/Services";
+import ServicesSection from "@/components/shared/ServicesSection";
+import { servicesArray } from "@/constant/services";
+import { useSearchParams } from "next/navigation";
+import React from "react";
 
 const Service = () => {
-    const searchParams = useSearchParams();
-    let NameService=searchParams.get("s")
-  return (
-      NameService!==undefined&&NameService!==null?
-    <LoanApplication  NameService={NameService} />:
-    <div className=' flex flex-col items-center'>
-        <ServicesSection />
-      {servicesArray.map(service =>
-        <Services key={service.img} img={service.img} title={service.title} subTitle={service.subTitle} link={service.link} desc={service.desc} />
-    ) }
+  const searchParams = useSearchParams();
+  let NameService = searchParams.get("s");
+  return NameService !== undefined && NameService !== null ? (
+    <LoanApplication NameService={NameService} />
+  ) : (
+    <div className=" flex flex-col items-center">
+      <ServicesSection />
+      {servicesArray.map((service) => (
+        <Services
+          key={service.img}
+          img={service.img}
+          title={service.title}
+          subTitle={service.subTitle}
+          link={service.link}
+          desc={service.desc}
+        />
+      ))}
     </div>
+  );
+};
 
-  )
-}
-
-export default Service
+export default Service;

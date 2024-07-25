dc37dc169bbec8cf6aa84a407eb2cd153dd44389
Ahmed <ahmed15ayman7ahmed2002@gmail.com>
2024-07-25 14:00:55 +0300

    update

--- components/cards/ChatBotIcon.tsx
+++ components/cards/ChatBotIcon.tsx
@@ -3,9 +3,12 @@ import { MdChat } from "react-icons/md";
 const ChatIcon: React.FC = () => {
   return (
     <div className="fixed bottom-4 left-4">
+        <form action="https://hlolak-chatbot.vercel.app/">
+
       <button className="bg-white text-[#ab994e] p-4 rounded-full shadow-lg hover:ring-white/90 focus:outline-none focus:ring-2 focus:ring-white/90">
       <MdChat  />
       </button>
+        </form>
     </div>
   );
 };
